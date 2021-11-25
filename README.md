ebe159bc5d8ed4d16ffb53d329ca3911961ced8d
Manoj S H <msatha@innominds.com>
2021-11-25 18:14:43 +0530

    initial added files

---
+++ README.md
@@ -0,0 +1,2 @@
+# flask-projectt
+Flask project
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import *
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+mongo_client=PyMongo(app,uri="mongodb+srv://manoj:manoj@cluster0.gnr87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = mongo_client.db
+
+@app.route('/login',methods = ['POST','GET'])
+def login():
+    if(request.method == 'POST'):
+        if(request.form["userName"] =="spa"):
+            db.flask.insert_one(dict(request.form))
+            return redirect(url_for('success'))
+        else:
+            return redirect(url_for('indexsignup'))
+    else:
+        return render_template('login.html') 
+
+
+@app.route('/signup',methods = ['POST','GET'])
+def signup():
+    if(request.method == 'POST'):
+        if(request.form):
+            db.flask.insert_one(dict(request.form))
+            return redirect(url_for('success'))
+        else:
+            return redirect(url_for('indexsignup'))
+    else:
+        return render_template('sign_up.html') 
+
+@app.route('/success')
+def success():
+   return 'logged in successfully'
+
+@app.route('/signup')
+def indexsignup():
+    return render_template('sign_up.html')
+
+
+
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template('forgot_password.html')
+
+if __name__ == "__main__" :
+    app.run(debug=True)
